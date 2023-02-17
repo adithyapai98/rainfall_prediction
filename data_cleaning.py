5c2fd6bf8a35be6559e71e0991632bba75216aa7
adithyapai98 <125631375+adithyapai98@users.noreply.github.com>
2023-02-17 11:54:47 +0530

    added comments to the code

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,11 @@
 import pandas as pd
+#read the source file
 df = pd.read_csv('C:/Users/adith/Desktop/Analysis/weather_source.csv',skiprows=3)
+#clean the column names
 df.columns = df.columns.str.replace('\(.*\)', '',regex=True).str.strip()
+#drop time and weathercode columns
 data=df.drop(['time','weathercode'],axis=1)
+#drop NaN values
 data=data.dropna()
+#store the processed file in csv format
 data.to_csv('C:/Users/adith/Desktop/Analysis/weather_clean.csv',index=False)
